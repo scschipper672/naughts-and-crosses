f5cf71d3ee3d5e760af5e7c14f526f8e7d653d97
sams <scschipper672@gmail.com>
2018-09-13 19:57:44 +0900

    Initial commit

---
+++ naughts_and_crosses.py
@@ -0,0 +1,7 @@
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+board_string = f'    {board[0][0]} | {board[0][1]} | {board[0][2]}\n\
+   ---+---+---\n\
+    {board[1][0]} | {board[1][1]} | {board[1][2]}\n\
+   ---+---+---\n\
+    {board[2][0]} | {board[2][1]} | {board[2][2]}'
+print(board_string)
